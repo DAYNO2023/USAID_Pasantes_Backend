5f81c7069e47d25dd19cdc8f5e757af513dd2eb6
seucedaa <134754822+seucedaa@users.noreply.github.com>
2024-10-30 08:11:32 -0600

    feat(clases): clases a carpetas

---
+++ USAID_Pasantes.BusinessLogic/Services/ServicesAcceso/RolService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USAID_Pasantes.BusinessLogic.Services.ServicesAcceso
+{
+    class RolService
+    {
+    }
+}
