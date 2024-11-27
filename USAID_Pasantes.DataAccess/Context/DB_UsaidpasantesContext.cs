1e68071e5c557ecd1bfda0cb94732e8269bcf33b
Yordin Sanchez <146747575+DAYNO2023@users.noreply.github.com>
2024-11-26 20:40:44 -0600

    docs

--- USAID_Pasantes.DataAccess/Context/DB_UsaidpasantesContext.cs
+++ USAID_Pasantes.DataAccess/Context/DB_UsaidpasantesContext.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using USAID_Pasantes.DataAccess;
+using USAID_Pasantes.Entities.Entities;
 
 #nullable disable
 
